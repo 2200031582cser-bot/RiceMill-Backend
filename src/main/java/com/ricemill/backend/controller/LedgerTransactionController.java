82577d07ab8669a4593c6c9aae22c171ad722889
Vishnu Kasam <2200031582cser@gmail.com>
2026-07-23 22:16:23 +0530

    Update LedgerTransactionController.java

--- src/main/java/com/ricemill/backend/controller/LedgerTransactionController.java
+++ src/main/java/com/ricemill/backend/controller/LedgerTransactionController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@CrossOrigin("*")
+
 @RequestMapping("/ledger-transaction")
 public class LedgerTransactionController {
 
