9926f0597e61f74feee34424d9b7830a65b50cdb
Vishnu Kasam <2200031582cser@gmail.com>
2026-07-23 22:14:45 +0530

    Remove CrossOrigin annotation from CompanyProfileController

--- src/main/java/com/ricemill/backend/controller/CompanyProfileController.java
+++ src/main/java/com/ricemill/backend/controller/CompanyProfileController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/company-profile")
-@CrossOrigin(origins = "*")
+
 public class CompanyProfileController {
 
     @Autowired
